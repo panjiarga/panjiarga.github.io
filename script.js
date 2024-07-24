90c0fd686d6a3fdf7f7ce42f9aebe053ef9993a0
panjiarga <153212139+panjiarga@users.noreply.github.com>
2024-07-24 06:27:50 +0100

    added responsive for mobile, 1x1 grid options, plus a couple of projects

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 
-const data = [
+const mydata = [
     {
-        name: "Ganesha Film Festival",
-        type: "Brand Direction",
-        images: {
-            main: './images/ganffest.png'
-        },
-        color: {
-            primary: 'white',
-            text:'red'
-        }
-    },
-    {
+        width: 2,
         name: "Maka Motors",
         type: "Brand Design",
+        year: 2023,
         images: {
-            main: './images/maka.png'
+            full: './images/maka.png'
         },
         color: {
             primary: '#55CCFF',
@@ -23,10 +14,25 @@ const data = [
         }
     },
     {
+        width: 2,
+        name: "Queerdo",
+        type: "Art Performance",
+        year: 2023,
+        images: {
+            full: './images/queerdo.png'
+        },
+        color: {
+            primary: '#590D31',
+            text:'white'
+        }
+    },
+    {
+        width: 2,
         name: "GO-BOX",
         type: "Brand Direction",
+        year: 2016,
         images: {
-            main: './images/gobox.png'
+            full: './images/gobox.png'
         },
         color: {
             primary: '#E2B100',
@@ -34,27 +40,79 @@ const data = [
         }
     },
     {
-        name: "Queerdo",
-        type: "Art Performance",
+        width: 1,
+        name: "Ganesha Exhibition",
+        type: "Logo Design",
+        year: 2013,
         images: {
-            main: './images/queerdo.png'
+            png: './images/gep_logo.png'
         },
         color: {
-            primary: '#590D31',
+            primary: '#203B8C',
+            text:'white'
+        }
+    },
+    {
+        width: 2,
+        name: "Ganesha Film Festival",
+        type: "Brand Direction",
+        year: 2013,
+        images: {
+            full: './images/ganffest.png'
+        },
+        color: {
+            primary: 'white',
+            text:'red'
+        }
+    },
+    {
+        width: 1,
+        name: "EV",
+        type: "Illustration",
+        year: 2023,
+        images: {
+            png: './images/motorcycle.png'
+        },
+        color: {
+            primary: 'black',
+            text:'#55CCFF'
+        }
+    },
+    {
+        width: 1,
+        name: "Circular Maze Generator",
+        type: "Code",
+        year: 2024,
+        images: {
+            full: './images/maze.png'
+        },
+        color: {
+            primary: 'black',
             text:'white'
         }
     }
 ];
 
+let data = [];
+for (let d of mydata) {
+    if (Math.round(Math.random())) {
+        data.push(d);
+    } else {
+        data.unshift(d);
+    }
+}
+
 for (let i=0;i<data.length;i++) {
     const item = document.createElement("div");
     item.setAttribute("class", "items");
+    item.style.flex = ""+(data[i].width * 20)+"%";
+    item.style.maxWidth = ""+((25*data[i].width)-1)+"%";
 
     const content = document.createElement("div");
     content.setAttribute("class", "content");
 
     const pname = document.createElement("span");
-    pname.setAttribute("class","title");
+    pname.setAttribute("class","name");
     pname.innerHTML = data[i].name;
     content.appendChild(pname);
 
@@ -63,12 +121,17 @@ for (let i=0;i<data.length;i++) {
     ptype.innerHTML = data[i].type;
     content.appendChild(ptype);
 
-    item.appendChild(content);
-
-    item.style.backgroundColor=data[i].color.primary;
-    if (data[i].images.main) {
-        item.style.backgroundImage="url("+data[i].images.main+")";
+    if (data[i].images.full) {
+        item.style.backgroundImage="url("+data[i].images.full+")";
     }
+    if (data[i].images.png) {
+        const img = document.createElement('img');
+        img.setAttribute("src",data[i].images.png);
+        content.appendChild(img);
+    }
+    
+    item.appendChild(content);
+    item.style.backgroundColor=data[i].color.primary;
     item.style.color=data[i].color.text;
     //console.log(document.getElementById("collection"));
     document.getElementById("collection").appendChild(item);
