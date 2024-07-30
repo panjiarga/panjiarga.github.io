d9cc8cb9af698cf54c6f73dfa086517aeb3e417c
panjiarga <153212139+panjiarga@users.noreply.github.com>
2024-07-30 08:15:41 +0100

    minor content changes

--- script.js
+++ script.js
@@ -140,11 +140,11 @@ const mydata = [
             primary: '#13889D',
             text:'#F9CF55'
         }
-    },{width: 3,name: "Travel Photography",
+    },{width: 3,name: "Photography",
         type: "",
         year: 2024,
         images: {
-            full: './images/photography/photo'+Math.ceil(Math.random()*15)+'.jpg'
+            full: './images/photography/photo'+(Math.floor(Math.random()*14)+1)+'.jpg'
         },
         color: {
             primary: 'grey',
@@ -170,17 +170,48 @@ const mydata = [
             primary: 'dark grey',
             text:'white'
         }
+    },{width: 2,name: "Icarus",
+        type: "Game",
+        year: 2024,
+        images: {
+            full: './images/icarus.png'
+        },
+        color: {
+            primary: 'black',
+            text:'white'
+        }
+    },{width: 2,name: "Colors of India",
+        type: "Videography",
+        year: 2014,
+        images: {
+            full: './images/colorsofindia.png',
+            video: './images/colorsofindia.mp4'
+        },
+        color: {
+            primary: 'black',
+            text:'white'
+        }
     }
 ];
 
-let data = [];
-for (let d of mydata) {
-    if (Math.round(Math.random())) {
-        data.push(d);
-    } else {
-        data.unshift(d);
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
     }
+  
+    return array;
 }
+data = shuffle(mydata);  
 
 function createDiv (item) {
     const element = document.createElement("div");
@@ -219,6 +250,17 @@ function createDiv (item) {
         img.setAttribute("src",item.images.png);
         content.appendChild(img);
     }
+    if (item.images.video) {
+        const vid = document.createElement('video');
+        const src = document.createElement('source');
+        vid.setAttribute("autoplay","");
+        vid.setAttribute("loop","");
+        vid.setAttribute("muted","");
+        src.setAttribute("src",item.images.video);
+        src.setAttribute("type","video/mp4");
+        vid.appendChild(src);
+        content.appendChild(vid);
+    }
 
     element.appendChild(content);
     element.style.backgroundColor=item.color.primary;
@@ -234,8 +276,8 @@ function wrapDiv (elements,flexdir) {
     console.log('wrapDiv for');
     console.log(elements);
     elements = elements.sort(WidthSort);
-    for (let i = 0; i < elements.length; i++) {
-        parent.appendChild(createDiv(elements[i]));
+    for (const el of elements) {
+        parent.appendChild(createDiv(el));
     }
     return parent;
 }
@@ -250,110 +292,113 @@ function WidthSort (a,b) {
 }
 
 
-let creation = [];
-let x;
+
 const collection = document.getElementById("collection");
-while (data.length>0) {
-    x = data.shift(); //add first square
-    creation.push(x);
+
+let elements = [];
+let x;
+while (data.length) {
+    //add first square
+    elements.push(data.shift());
     console.log('1st');
-    console.log(creation[0]);
-    if (data.length>=1) { //can add second square
-        let idx = data.findIndex(function(el){return (el.width==creation[0].width);}); //find another square with the same width
+    console.log(elements[0]);
+    if (data.length) { //can add second square
+        let idx = data.findIndex((el)=>(el.width==elements[0].width)); //find another square with the same width
+        
         if (idx!=-1) { //found the same square
             x = data.splice(idx,1)[0];
-            creation.push(x);
+            elements.push(x);
             console.log('2nd');
-            console.log(creation[1]);
+            console.log(elements[1]);
             //find third square?
-            if (creation[0].width==1) { //two little square
+            if (elements[0].width==1) { //two little square
                 idx = data.findIndex(function(el){return (el.width!=1);}); //find a big square
                 if (idx!=-1) { //found a big square
                     x = data.splice(idx,1)[0];
-                    creation.push(x);
+                    elements.push(x);
                     console.log('3rd');
-                    console.log(creation[2]);
-                    collection.appendChild(wrapDiv(creation,(creation[2].width == 3 ? 'vertical':'horizontal')));
+                    console.log(elements[2]);
+                    collection.appendChild(wrapDiv(elements,(elements[2].width == 3 ? 'vertical':'horizontal')));
                     console.log('bbP');
                 } else { //no big square, all little square left
                     if (data.length>0) {
                         x = data.shift();
-                        creation.push(x);
+                        elements.push(x);
                         console.log('3rd');
-                        console.log(creation[2]); // third little square
+                        console.log(elements[2]); // third little square
                         if (data.length>0) { //there are more little square 
                             x = data.shift();
-                            creation.push(x);
+                            elements.push(x);
                             console.log('3rd');
-                            console.log(creation[3]); // fourth little square
+                            console.log(elements[3]); // fourth little square
                         }
                     }
                     console.log('bbbb');
-                    collection.appendChild(wrapDiv(creation,'horizontal')); 
+                    collection.appendChild(wrapDiv(elements,'horizontal')); 
                 }
             } else { //good day
                 console.log('PP');
-                collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+                collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
             }
         } else { //no same square left
-            if (creation[0].width!=1) { //if the only big square left
+            if (elements[0].width!=1) { //if the only big square left
                 //can we find two little square?
                 idx = data.findIndex(function(el){return (el.width==1);}); //find a little square
                 if (idx!=-1) { //found lil square (2nd)
                     x = data.splice(idx,1)[0];
-                    creation.push(x);
+                    elements.push(x);
                     console.log('2nd');
-                    console.log(creation[1]); //second little square
+                    console.log(elements[1]); //second little square
                     if (data.length>0) { //try to find third little square
                         idx = data.findIndex(function(el){return (el.width==1);}); //find a little square
                         if (idx!=-1) { //found lil square (3rd)
                             x = data.splice(idx,1)[0];
-                            creation.push(x);
+                            elements.push(x);
                             console.log('3rd');
-                            console.log(creation[2]); //third little square
-                            collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+                            console.log(elements[2]); //third little square
+                            collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
                             console.log('Ppp');
                         } else {
-                            creation[1].width = creation[0].width; //extend the second square to match
-                            collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+                            elements[1].width = elements[0].width; //extend the second square to match
+                            collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
                             console.log('P$');   
                         }
                     } else { //no lil square left...
-                        //creation[1].width = creation[0].width;
-                        collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+                        //elements[1].width = elements[0].width;
+                        collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
                         console.log('P$');
                     }
                 } else { //the rest is the other big square
                     //pop back the thing
                     if (data.length >= 2) {
                         console.log('reject');
-                        console.log(creation[0]);
-                        data.push(creation[0]);
+                        console.log(elements[0]);
+                        data.push(elements[0]);
                     } else {
-                        collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
-                        creation[0] = data.shift();
-                        collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+                        collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
+                        elements[0] = data.shift();
+                        collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
                     }
                 }
             } else { // the only lil square left
                 if (data.length <2) {
                     x = data.shift();
-                    creation.push(x);
+                    elements.push(x);
                     console.log('2nd');
-                    console.log(creation[1]);
-                    collection.appendChild(wrapDiv(creation,(creation[1].width == 3 ? 'vertical':'horizontal')));
+                    console.log(elements[1]);
+                    collection.appendChild(wrapDiv(elements,(elements[1].width == 3 ? 'vertical':'horizontal')));
                     console.log('Pp');
                 } else {
                     console.log('reject');
-                    console.log(creation[0]);
-                    data.push(creation[0]);
+                    console.log(elements[0]);
+                    data.push(elements[0]);
                 }
             }
         }
     } else {
-        collection.appendChild(wrapDiv(creation,(creation[0].width == 3 ? 'vertical':'horizontal')));
+        collection.appendChild(wrapDiv(elements,(elements[0].width == 3 ? 'vertical':'horizontal')));
         console.log('?');
     };
-    creation = [];
+    elements = [];
 }
 
